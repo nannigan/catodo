75e74ec9ffebb0957c0c1df221b14b172a8efa3b
Nan Davenport <nannigan@nannigans.com>
2014-12-09 19:13:41 +0000

    datepicker take three

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,7 +1,21 @@
- class DatepickerInput < SimpleForm::Inputs::Base 
-	 def input 
-		@builder.text_field(attribute_name, input_html_options) + \ 
-		@builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"}) 
-	 end 
- end 
+ # class DatepickerInput < SimpleForm::Inputs::Base 
+	#  def input 
+	# 	@builder.text_field(attribute_name, input_html_options)
+	# 	@builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"}) 
+	#  end 
+ # end 
+
+module SimpleForm
+  module Inputs
+    class DatePickerInput < Base
+      def input
+        @builder.text_field(attribute_name,input_html_options)
+      end    
+    end
+  end
+end
+
+
+
+
 
